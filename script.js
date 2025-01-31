4b94864f668d358dfb2e7e4521cc38c7d04606ad
Veronica Loza <lozav919@newschool.edu>
2025-01-30 20:35:09 -0500

    changes

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const textcontainer = document.getElementById("textcontainer");
-let easeFactor = 
+let easeFactor = 0.02;
+let scene, camera, renderer, planeMesh;
+let mousPoisiton = { x: 0.5, y: 0.5 };
+let prevPosition = { x: 0.5, y: 0.5 };
+const vertexShader = `
+
