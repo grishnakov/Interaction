b42f045b998d1570a4b12b4eb12c15bb76f9c98c
Veronica Loza <lozav919@newschool.edu>
2025-02-03 00:32:35 -0500

    dark mode

--- page-two/script.js
+++ page-two/script.js
@@ -1,6 +1,7 @@
 const toggleButton = document.getElementById("text");
 
 toggleButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
     toggleButton.textContent = toggleButton.textContent === "ON" ? "OFF" : "ON";
 });
 
