17acb0fa8644d1030b10ed1fff99e0c9fe6f4174
Veronica Loza <lozav919@newschool.edu>
2025-05-01 01:05:13 -0400

    hello

--- cafe-concept/script.js
+++ cafe-concept/script.js
@@ -1,4 +1,14 @@
-
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
 
 const imgWidth = 6583, imgHeight = 16838;
 const anchorPx = [3048, 11835];
